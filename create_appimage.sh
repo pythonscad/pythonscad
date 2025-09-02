55477fb13d01efd8c96a1f2de510eacb605e59b1
nomike <nomike@nomike.com>
2025-09-03 01:05:40 +0200

    Fix: Ensure script exits on error and clean up code style

--- create_appimage.sh
+++ create_appimage.sh
@@ -1,9 +1,8 @@
-#! /bin/sh
+#!/bin/sh -e
 export PYTHON_VERSION=$(python3 -c "import sys; print('.'.join(map(str, sys.version_info[:2])))")
 cd build
 cmake  -DCMAKE_INSTALL_PREFIX=/usr -DEXPERIMENTAL=1 -DENABLE_PYTHON=1 -DPYTHON_VERSION=${PYTHON_VERSION} -DENABLE_LIBFIVE=1 ..
 make -j3
-if [ ! $? == 0 ] ; then exit 1 ; fi
 rm -rf ../AppDir
 make install DESTDIR=../AppDir
 cd ..
