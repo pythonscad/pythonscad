52d1848af1e5fa0a9f5654c47004e2e476da4f33
nomike <nomike@nomike.com>
2025-09-02 03:45:09 +0200

    Fix: Synchronize package list between uni-get-dependencies.sh and github-ci-linux-get-dependencies.sh and

--- scripts/github-ci-linux-get-dependencies.sh
+++ scripts/github-ci-linux-get-dependencies.sh
@@ -3,10 +3,20 @@
 QT="$1"
 
 DIST=$(. /etc/os-release; echo $VERSION_CODENAME)
+PACKAGES1="build-essential bison flex git curl cmake ninja-build libffi-dev"
+PACKAGES2="libboost-program-options-dev libboost-regex-dev libboost-system-dev"
+PACKAGES3="libmpfr-dev libglew-dev libcairo2-dev libharfbuzz-dev libeigen3-dev"
+PACKAGES4="libcgal-dev libopencsg-dev libgmp-dev imagemagick libfreetype-dev"
+PACKAGES5="libdouble-conversion-dev libxml2-dev gtk-doc-tools libglib2.0-dev"
+PACKAGES6="gettext xvfb pkg-config ragel libtbb-dev libgl1-mesa-dev libxi-dev"
+PACKAGES7="libxmu-dev libfontconfig-dev libzip-dev libjpeg-dev libjpeg-dev"
+PACKAGES8="python3-dev nettle-dev python3-venv libcurl4-openssl-dev"
+PACKAGES9="libmimalloc-dev python3-dev libqt5gamepad5-dev python3-setuptools"
+
 
 PACKAGES1="build-essential bison cmake curl flex gettext git imagemagick ghostscript libcurl4-openssl-dev"
 PACKAGES2="libboost-program-options-dev libboost-regex-dev libboost-system-dev libeigen3-dev nettle-dev"
-PACKAGES3="libxi-dev libxmu-dev qtbase5-dev qtmultimedia5-dev libqt5opengl5-dev libqt5svg5-dev libqscintilla2-qt5-dev"
+PACKAGES3="libxi-dev libxmu-dev qtbase5-dev qtmultimedia5-dev libqt5opengl5-dev libqt5svg5-dev libqscintilla2-qt5-dev qt6-base-dev qt6-multimedia-dev libqt6core5compat6-dev libqt6svg6-dev libqscintilla2-qt6-dev"
 PACKAGES4="libcairo2-dev libcgal-dev libglew-dev libgmp-dev libmpfr-dev libegl-dev libegl1-mesa-dev libxml2-dev"
 PACKAGES5="libdouble-conversion-dev libfontconfig-dev libharfbuzz-dev libopencsg-dev lib3mf-dev libtbb-dev libzip-dev"
 
@@ -57,4 +67,4 @@ else
 
 fi
 
-sudo apt-get install -qq $PACKAGES1 $PACKAGES2 $PACKAGES3 $PACKAGES4 $PACKAGES5 $PACKAGES6
+sudo apt-get install -qq $PACKAGES1 $PACKAGES2 $PACKAGES3 $PACKAGES4 $PACKAGES5 $PACKAGES6 $PACKAGES7 $PACKAGES8 $PACKAGES9
