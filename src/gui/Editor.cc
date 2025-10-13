21a5caef40b0848b9923bb88f58c02ae3504d0ec
Guenther Sohler <guenther.sohler@photeon.com>
2025-10-13 21:25:24 +0200

    working now reliably

--- src/gui/Editor.cc
+++ src/gui/Editor.cc
@@ -19,7 +19,6 @@ void EditorInterface::wheelEvent(QWheelEvent *event)
 
 void EditorInterface::recomputeLanguageActive()
 {
-  printf("RecomputeLang\n");
   auto fnameba = filepath.toLocal8Bit();
   const char *fname = filepath.isEmpty() ? "" : fnameba;
 
@@ -28,16 +27,14 @@ void EditorInterface::recomputeLanguageActive()
   if (fname != NULL) {
 #ifdef ENABLE_PYTHON
     if (boost::algorithm::ends_with(fname, ".py")) {
-      std::string content = toPlainText().toStdString();
-      /* if (trust_python_file(std::string(fname), content)) */ language = LANG_PYTHON;  // TODO activate
-      // else LOG(message_group::Warning, Location::NONE, "", "Python is not enabled");
+      language = LANG_PYTHON;
     }
 #endif
   }
 
 #ifdef ENABLE_PYTHON
   if (oldLanguage != language) {
-    onLanguageActiveChanged(language);
+    onLanguageChanged(language);
   }
 #endif
 }
