48404edd67b5671d070051e3da68ad37ea79e017
nomike <nomike@nomike.com>
2026-01-30 02:48:44 +0100

    feat(gui): add Python-only call tips for openscad API
    Add call tip entries for Python-only functions (right, left, back, front,
    up, down, rotx, roty, rotz, separate, export, path_extrude, skin, concat,
    highlight, background, only, sheet, inside, bbox, size, position, faces,
    children, edges, explode, oversample, debug, repair, fillet, group,
    osimport, osuse, osinclude, version, version_num, scad, add_menuitem,
    nimport, model, modelpath) so parameter hints show in the editor.

--- src/gui/PythonApi.cc
+++ src/gui/PythonApi.cc
@@ -103,6 +103,49 @@ PythonApi::PythonApi(ScintillaEditor *editor, QsciLexerPython *lexer)
   // Python-only openscad names with call tips
   funcs.append(PythonApiFunc{"show", {"show(obj)", "show(obj, ...)"}});
   funcs.append(PythonApiFunc{"add_parameter", {"add_parameter(name, default, ...)"}});
+  funcs.append(PythonApiFunc{"right", {"right(x)", "right(obj, x)"}});
+  funcs.append(PythonApiFunc{"left", {"left(x)", "left(obj, x)"}});
+  funcs.append(PythonApiFunc{"back", {"back(x)", "back(obj, x)"}});
+  funcs.append(PythonApiFunc{"front", {"front(x)", "front(obj, x)"}});
+  funcs.append(PythonApiFunc{"up", {"up(x)", "up(obj, x)"}});
+  funcs.append(PythonApiFunc{"down", {"down(x)", "down(obj, x)"}});
+  funcs.append(PythonApiFunc{"rotx", {"rotx(angle)", "rotx(obj, angle)"}});
+  funcs.append(PythonApiFunc{"roty", {"roty(angle)", "roty(obj, angle)"}});
+  funcs.append(PythonApiFunc{"rotz", {"rotz(angle)", "rotz(obj, angle)"}});
+  funcs.append(PythonApiFunc{"separate", {"separate(obj)"}});
+  funcs.append(PythonApiFunc{"export", {"export(obj)", "export(obj, file=...)"}});
+  funcs.append(PythonApiFunc{"find_face", {"find_face(obj, normal)"}});
+  funcs.append(PythonApiFunc{"sitonto", {"sitonto(obj, x, y, z)"}});
+  funcs.append(PythonApiFunc{"path_extrude", {"path_extrude(shape, path, ...)"}});
+  funcs.append(PythonApiFunc{"skin", {"skin(profiles, ...)"}});
+  funcs.append(PythonApiFunc{"concat", {"concat(*objects)", "concat(*objects, r=..., fn=...)"}});
+  funcs.append(PythonApiFunc{"highlight", {"highlight(obj)"}});
+  funcs.append(PythonApiFunc{"background", {"background(obj)"}});
+  funcs.append(PythonApiFunc{"only", {"only(obj)"}});
+  funcs.append(PythonApiFunc{"sheet", {"sheet(file, ...)"}});
+  funcs.append(PythonApiFunc{"inside", {"inside(obj, point)"}});
+  funcs.append(PythonApiFunc{"bbox", {"bbox(obj)"}});
+  funcs.append(PythonApiFunc{"size", {"size(obj)"}});
+  funcs.append(PythonApiFunc{"position", {"position(obj)"}});
+  funcs.append(PythonApiFunc{"faces", {"faces(obj)"}});
+  funcs.append(PythonApiFunc{"children", {"children(obj)"}});
+  funcs.append(PythonApiFunc{"edges", {"edges(obj)"}});
+  funcs.append(PythonApiFunc{"explode", {"explode(obj, vector)"}});
+  funcs.append(PythonApiFunc{"oversample", {"oversample(obj, n, round=...)"}});
+  funcs.append(PythonApiFunc{"debug", {"debug(obj, faces=...)"}});
+  funcs.append(PythonApiFunc{"repair", {"repair(obj)"}});
+  funcs.append(PythonApiFunc{"fillet", {"fillet(obj, r, sel, fn=...)"}});
+  funcs.append(PythonApiFunc{"group", {"group(*objects)"}});
+  funcs.append(PythonApiFunc{"osimport", {"osimport(file, ...)"}});
+  funcs.append(PythonApiFunc{"osuse", {"osuse(path)"}});
+  funcs.append(PythonApiFunc{"osinclude", {"osinclude(path)"}});
+  funcs.append(PythonApiFunc{"version", {"version()"}});
+  funcs.append(PythonApiFunc{"version_num", {"version_num()"}});
+  funcs.append(PythonApiFunc{"scad", {"scad(code)"}});
+  funcs.append(PythonApiFunc{"add_menuitem", {"add_menuitem(menuname, itemname, callback)"}});
+  funcs.append(PythonApiFunc{"nimport", {"nimport(url)"}});
+  funcs.append(PythonApiFunc{"model", {"model()"}});
+  funcs.append(PythonApiFunc{"modelpath", {"modelpath()"}});
 }
 
 void PythonApi::updateAutoCompletionList(const QStringList& context, QStringList& list)
