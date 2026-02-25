07f3261ab7b68c25ee6d4d153bda71e85bd2f058
John (EBo) David <ebo.2112@gmail.com>
2026-02-25 16:41:19 -0500

    added the alpha channel (0xFF) to each color in the colormap table.
    changed the derived/dynamic color to bit-mangling using the alpha
    channel - which bought us 6 extra bits of precision for the feed rate,
    and still provide a full 10-bits to the power (0..1000)..
    
    changed color2str, and related functions, to output 8 hex digits
    instead of 6 to account for the additional alpha bits.

--- libraries/python/pymachineconfig.py
+++ libraries/python/pymachineconfig.py
@@ -44,69 +44,69 @@ class MachineConfig:
 
     def gen_color_table(self):
         self.register("L00","ColorTable",
-                      {"power":1,"feed":1,"color":0x000000})
+                      {"power":1,"feed":1,"color":0x000000FF})
         self.register("L01","ColorTable",
-                      {"power":1,"feed":1,"color":0x0000FF})
+                      {"power":1,"feed":1,"color":0x0000FFFF})
         self.register("L02","ColorTable",
-                      {"power":1,"feed":1,"color":0xFF0000})
+                      {"power":1,"feed":1,"color":0xFF0000FF})
         self.register("L03","ColorTable",
-                      {"power":1,"feed":1,"color":0x00E000})
+                      {"power":1,"feed":1,"color":0x00E000FF})
         self.register("L04","ColorTable",
-                      {"power":1,"feed":1,"color":0xD0D000})
+                      {"power":1,"feed":1,"color":0xD0D000FF})
         self.register("L05","ColorTable",
-                      {"power":1,"feed":1,"color":0xFF8000})
+                      {"power":1,"feed":1,"color":0xFF8000FF})
         self.register("L06","ColorTable",
-                      {"power":1,"feed":1,"color":0x00E0E0})
+                      {"power":1,"feed":1,"color":0x00E0E0FF})
         self.register("L07","ColorTable",
-                      {"power":1,"feed":1,"color":0xFF00FF})
+                      {"power":1,"feed":1,"color":0xFF00FFFF})
         self.register("L08","ColorTable",
-                      {"power":1,"feed":1,"color":0xB4B4B4})
+                      {"power":1,"feed":1,"color":0xB4B4B4FF})
         self.register("L09","ColorTable",
-                      {"power":1,"feed":1,"color":0x0000A0})
+                      {"power":1,"feed":1,"color":0x0000A0FF})
         self.register("L10","ColorTable",
-                      {"power":1,"feed":1,"color":0xA00000})
+                      {"power":1,"feed":1,"color":0xA00000FF})
         self.register("L11","ColorTable",
-                      {"power":1,"feed":1,"color":0x00A000})
+                      {"power":1,"feed":1,"color":0x00A000FF})
         self.register("L12","ColorTable",
-                      {"power":1,"feed":1,"color":0xA0A000})
+                      {"power":1,"feed":1,"color":0xA0A000FF})
         self.register("L13","ColorTable",
-                      {"power":1,"feed":1,"color":0xC08000})
+                      {"power":1,"feed":1,"color":0xC08000FF})
         self.register("L14","ColorTable",
-                      {"power":1,"feed":1,"color":0x00A0FF})
+                      {"power":1,"feed":1,"color":0x00A0FFFF})
         self.register("L15","ColorTable",
-                      {"power":1,"feed":1,"color":0xA000A0})
+                      {"power":1,"feed":1,"color":0xA000A0FF})
         self.register("L16","ColorTable",
-                      {"power":1,"feed":1,"color":0x808080})
+                      {"power":1,"feed":1,"color":0x808080FF})
         self.register("L17","ColorTable",
-                      {"power":1,"feed":1,"color":0x7D87B9})
+                      {"power":1,"feed":1,"color":0x7D87B9FF})
         self.register("L18","ColorTable",
-                      {"power":1,"feed":1,"color":0xBB7784})
+                      {"power":1,"feed":1,"color":0xBB7784FF})
         self.register("L19","ColorTable",
-                      {"power":1,"feed":1,"color":0x4A6FE3})
+                      {"power":1,"feed":1,"color":0x4A6FE3FF})
         self.register("L20","ColorTable",
-                      {"power":1,"feed":1,"color":0xD33F6A})
+                      {"power":1,"feed":1,"color":0xD33F6AFF})
         self.register("L21","ColorTable",
-                      {"power":1,"feed":1,"color":0x8CD78C})
+                      {"power":1,"feed":1,"color":0x8CD78CFF})
         self.register("L22","ColorTable",
-                      {"power":1,"feed":1,"color":0xF0B98D})
+                      {"power":1,"feed":1,"color":0xF0B98DFF})
         self.register("L23","ColorTable",
-                      {"power":1,"feed":1,"color":0xF6C4E1})
+                      {"power":1,"feed":1,"color":0xF6C4E1FF})
         self.register("L24","ColorTable",
-                      {"power":1,"feed":1,"color":0xFA9ED4})
+                      {"power":1,"feed":1,"color":0xFA9ED4FF})
         self.register("L25","ColorTable",
-                      {"power":1,"feed":1,"color":0x500A78})
+                      {"power":1,"feed":1,"color":0x500A78FF})
         self.register("L26","ColorTable",
-                      {"power":1,"feed":1,"color":0xB45A00})
+                      {"power":1,"feed":1,"color":0xB45A00FF})
         self.register("L27","ColorTable",
-                      {"power":1,"feed":1,"color":0x004754})
+                      {"power":1,"feed":1,"color":0x004754FF})
         self.register("L28","ColorTable",
-                      {"power":1,"feed":1,"color":0x86FA88})
+                      {"power":1,"feed":1,"color":0x86FA88FF})
         self.register("L29","ColorTable",
-                      {"power":1,"feed":1,"color":0xFFDB66})
+                      {"power":1,"feed":1,"color":0xFFDB66FF})
         self.register("T1","ColorTable",
-                      {"power":1,"feed":1,"color":0xF36926})
+                      {"power":1,"feed":1,"color":0xF36926FF})
         self.register("T2","ColorTable",
-                      {"power":1,"feed":1,"color":0x0C96D9})
+                      {"power":1,"feed":1,"color":0x0C96D9FF})
 
     def read(self, name="PythonSCAD.json"):
         name = self.configfile(name)
@@ -294,43 +294,32 @@ class MachineConfig:
             print("\nError: cannot represent a power factor greater than 100.0%\n")
             return color
 
-        # FIXME: check the bit-shifts in export_gcode.cc and see if I
-        #   can include alpha.
-        #
-        # FIXME: there is an issue with the alpha channel. Use an
-        #  8-bit power for now.
-        if True:
-            power = int(power/4)
-            if power != -1: color |= (power << 24)
-            if feed  != -1: color |= (feed << 8)
-        else:
-            # power is 10-bits, but has a max of 1000.
-            if power != -1: color |= (power << 22)
-            #print(f"  -- power color: 0x{color:06X}")
+        # The alpha bits are mangled into the RGB values to encode a
+        # full 12-bits for for power levels 0..1000 and 20-bits for
+        # the feed rate.  In order to make the visualizaion of the
+        # alpha channel work, the top most 6-bits of the feed rate are
+        # shifted into the alpha channel.
+        if power != -1: color |= (power << 22)
 
-            # make sure that feed is clipped to size
-            cfeed = feed & 0x3FFFFF
-            a = (~(cfeed >> 16)) & 0xFF
-            gb = (cfeed & 0xFFFF) << 8
+        cfeed = feed & 0x3FFFFF
+        a = (~(cfeed >> 16)) & 0xFF
+        gb = (cfeed & 0xFFFF) << 8
 
-            if feed  != -1: color |= (gb | a)
-            #print(f"*****     a:0x{a:06X}  gb:0x{gb:06X}")
-            #print("*****     power:",power,"  feed:",feed)
-            #print(f"***** new color: 0x{color:06X}")
+        if feed  != -1: color |= (gb | a)
 
         return color
 
     # color2str - return the working labled color as an OpenSCAD
-    #   compatible string representation of the hex value starting with a
-    #   '#'
+    #   compatible string representation of the hex value starting
+    #   with a '#'
     def color2str(self, tag):
-        return "#{:06X}".format(self.color(tag))
+        return "#{:08X}".format(self.color(tag))
 
     def gen_color2str(self, power=-1,feed=-1):
         color = self.gen_color(power,feed)
-        return f"#{color:06X}"
+        return f"#{color:08X}"
 
     def gen_color2hex(self, power=-1,feed=-1):
         color = self.gen_color(power,feed)
-        return f"0x{color:06X}"
+        return f"0x{color:08X}"
 
