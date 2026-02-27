bd960d84fd54c246f49f01fbd33235d364d33883
John (EBo) David <ebo.2112@gmail.com>
2026-02-27 13:40:26 -0500

    fix: remove configfile from the GUI

--- src/gui/ExportGcodeDialog.cc
+++ src/gui/ExportGcodeDialog.cc
@@ -5,19 +5,16 @@
 ExportGcodeDialog::ExportGcodeDialog()
 {
   setupUi(this);
-  this->laserSpeed=Settings::SettingsExportGcode::exportGcodeFeedRate.value();
-  this->laserPower=Settings::SettingsExportGcode::exportGcodeLaserPower.value();
-  this->laserMode =Settings::SettingsExportGcode::exportGcodeLaserMode.value();
-  this->initCode =QString(Settings::SettingsExportGcode::exportGcodeInitCode.value().c_str());
-  this->exitCode =QString(Settings::SettingsExportGcode::exportGcodeExitCode.value().c_str());
-  this->configfile =QString(Settings::SettingsExportGcode::exportGcodeConfigFile.value().c_str());
+  this->laserSpeed = Settings::SettingsExportGcode::exportGcodeFeedRate.value();
+  this->laserPower = Settings::SettingsExportGcode::exportGcodeLaserPower.value();
+  this->laserMode = Settings::SettingsExportGcode::exportGcodeLaserMode.value();
+  this->initCode = QString(Settings::SettingsExportGcode::exportGcodeInitCode.value().c_str());
+  this->exitCode = QString(Settings::SettingsExportGcode::exportGcodeExitCode.value().c_str());
   valueLaserSpeed->setText(QString::number(this->laserSpeed));
   valueLaserPower->setText(QString::number(this->laserPower));
   valueInitCode->setText(this->initCode);
   valueExitCode->setText(this->exitCode);
-  valueConfigFile->setText(this->configfile);
   valueLaserMode->setCurrentIndex(this->laserMode);
-
 }
 
 int ExportGcodeDialog::exec()
@@ -26,8 +23,7 @@ int ExportGcodeDialog::exec()
   if ((QApplication::keyboardModifiers() & Qt::ShiftModifier) != 0) {
     showDialog = true;
   }
-   return showDialog ? QDialog::exec() : QDialog::Accepted;
-	
+  return showDialog ? QDialog::exec() : QDialog::Accepted;
 }
 
 double ExportGcodeDialog::getLaserSpeed() const
@@ -39,24 +35,19 @@ double ExportGcodeDialog::getLaserPower() const
   return laserPower;
 }
 
-int ExportGcodeDialog::getLaserMode()  const
+int ExportGcodeDialog::getLaserMode() const
 {
   return laserMode;
 }
 
-QString ExportGcodeDialog::getInitCode()  const
+QString ExportGcodeDialog::getInitCode() const
 {
   return initCode;
 }
 
-QString ExportGcodeDialog::getExitCode()  const
+QString ExportGcodeDialog::getExitCode() const
 {
   return exitCode;
-}
-
-QString ExportGcodeDialog::getConfigFile()  const
-{
-  return configfile;
 }
 
 ExportGcodeOptions ExportGcodeDialog::getOptions()
@@ -67,53 +58,40 @@ ExportGcodeOptions ExportGcodeDialog::getOptions()
   opts.lasermode = getLaserMode();
   opts.initCode = getInitCode().toStdString();
   opts.exitCode = getExitCode().toStdString();
-  opts.configfile = getConfigFile().toStdString();
   Settings::SettingsExportGcode::exportGcodeFeedRate.setValue(opts.feedrate);
   Settings::SettingsExportGcode::exportGcodeLaserPower.setValue(opts.laserpower);
   Settings::SettingsExportGcode::exportGcodeLaserMode.setValue(opts.lasermode);
   Settings::SettingsExportGcode::exportGcodeInitCode.setValue(opts.initCode);
   Settings::SettingsExportGcode::exportGcodeExitCode.setValue(opts.exitCode);
-  Settings::SettingsExportGcode::exportGcodeConfigFile.setValue(opts.configfile);
   writeSettings();
   return opts;
 }
 
-
 void ExportGcodeDialog::on_valueLaserSpeed_textChanged(const QString& str)
 {
-  this->laserSpeed  = str.toDouble();	
-
+  this->laserSpeed = str.toDouble();
 }
 void ExportGcodeDialog::on_valueLaserPower_textChanged(const QString& str)
 {
-  this->laserPower = str.toDouble();	
+  this->laserPower = str.toDouble();
 }
 
 void ExportGcodeDialog::on_valueLaserMode_activated(int ind)
 {
-  this->laserMode = ind;	
-
+  this->laserMode = ind;
 }
 
 void ExportGcodeDialog::on_valueInitCode_textChanged(void)
 {
-  this->initCode = valueInitCode->toPlainText();	
+  this->initCode = valueInitCode->toPlainText();
 }
 
 void ExportGcodeDialog::on_valueExitCode_textChanged(void)
 {
-  this->exitCode = valueExitCode->toPlainText();	
+  this->exitCode = valueExitCode->toPlainText();
 }
-
-
-void ExportGcodeDialog::on_valueConfigFile_textChanged(void)
-{
-  this->configfile = valueConfigFile->toPlainText();
-}
-
 
 void ExportGcodeDialog::on_pushButtonCancel_clicked()
 {
   reject();
 }
-
