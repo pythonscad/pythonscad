2c1225e4e750ab6cb1b43fc16dea7c01dfab10de
Guenther Sohler <guenther.sohler@photeon.com>
2026-09-18 14:56:49 +0200

    fix(chore): make old loft available
    at the same time

--- setup.py
+++ setup.py
@@ -727,6 +727,7 @@ def main():
               "src/geometry/roof_ss.cc",
               "src/geometry/roof_vd.cc",
               "src/geometry/skin.cc",
+              "src/geometry/loft.cc",
               "src/geometry/linear_extrude.cc",
               "src/geometry/organic_resample.cc",
               "src/geometry/cgal/CGALCache.cc",
@@ -793,6 +794,7 @@ def main():
               "src/core/PathExtrudeNode.cc",
               "src/core/GroupModule.cc",
               "src/core/SkinNode.cc",
+              "src/core/LoftNode.cc",
               "src/core/RepairNode.cc"
             ]
     context = [
